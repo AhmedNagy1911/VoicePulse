a5a9b3f14c3956406cdc7a7a41e8b0170cc886e2
AhmedNagy1911 <zamalekahmed196@gmail.com>
2026-08-21 02:36:24 +0300

    add  Parameter Binding

--- VoicePulse.API/Controllers/PollsController.cs
+++ VoicePulse.API/Controllers/PollsController.cs
@@ -5,7 +5,7 @@ using VoicePulse.Domain.Entities;
 namespace VoicePulse.API.Controllers;
 
 [Route("api/[controller]")]
-[ApiController]
+//[ApiController]
 public class PollsController(IPollService pollService) : ControllerBase
 {
     private readonly IPollService _pollservice = pollService;
@@ -17,7 +17,7 @@ public class PollsController(IPollService pollService) : ControllerBase
     }
 
     [HttpGet("{id}")]
-    public IActionResult GetById(int id)
+    public IActionResult GetById([FromRoute] int id)
     {
         var poll = _pollservice.GetById(id);    
 
@@ -25,7 +25,7 @@ public class PollsController(IPollService pollService) : ControllerBase
     }
 
     [HttpPost("")]
-    public IActionResult Add(Poll poll)
+    public IActionResult Add([FromBody] Poll poll)
     {
         var newPoll = _pollservice.Add(poll);
 
@@ -33,14 +33,14 @@ public class PollsController(IPollService pollService) : ControllerBase
     }
 
     [HttpPut("{id}")]
-    public IActionResult Update(int id, Poll poll)
+    public IActionResult Update([FromRoute] int id, [FromBody] Poll poll)
     {
         var isUpdated = _pollservice.Update(id,poll);
 
         return isUpdated ? NoContent() : NotFound();
     }
     [HttpDelete("{id}")]
-    public IActionResult Delete (int id)
+    public IActionResult Delete ([FromRoute] int id)
     {
         var isDelete = _pollservice.Delete(id);
 
