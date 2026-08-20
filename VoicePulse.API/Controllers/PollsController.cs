b1645b289c6d81637651867573872c1b5bb51a47
AhmedNagy1911 <zamalekahmed196@gmail.com>
2026-08-20 19:20:55 +0300

    Add Polls Controller

---
+++ VoicePulse.API/Controllers/PollsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VoicePulse.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PollsController : ControllerBase
+{
+}
