c2dd3f6068a538303993fe0efdf4ffed471ee97e
AhmedNagy1911 <zamalekahmed196@gmail.com>
2026-08-20 20:52:55 +0300

    Add Get By Id Endpoint

--- VoicePulse.API/Controllers/PollsController.cs
+++ VoicePulse.API/Controllers/PollsController.cs
@@ -7,11 +7,27 @@ namespace VoicePulse.API.Controllers;
 [ApiController]
 public class PollsController : ControllerBase
 {
-    private readonly List<Poll> _poll = [];
+    private readonly List<Poll> _poll = 
+        [
+        new Poll{
+            Id = 1,
+            Title = "Poll 1",
+            Description = "Description for Poll 1"
+        }
 
-    [HttpGet]
+        ];
+
+    [HttpGet("")]
     public IActionResult GetAll()
     {
         return Ok(_poll);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var poll = _poll.SingleOrDefault(p => p.Id == id);
+      
+        return poll is not null ? Ok(poll) : NotFound();
     }
 }
