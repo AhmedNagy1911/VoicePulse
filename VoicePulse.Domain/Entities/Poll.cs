83d25c31364edd1f80348fb8daa6474f0f304bac
AhmedNagy1911 <zamalekahmed196@gmail.com>
2026-08-22 01:03:11 +0300

    Add Polls Configurations

--- VoicePulse.Domain/Entities/Poll.cs
+++ VoicePulse.Domain/Entities/Poll.cs
@@ -1,6 +1,6 @@
 ﻿namespace VoicePulse.Domain.Entities;
 
-public class Poll
+public sealed class Poll
 {
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
--- VoicePulse.Infrastructure/Persistence/ApplicationDbContext.cs
+++ VoicePulse.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 using VoicePulse.Application.Common.Interfaces;
 using VoicePulse.Domain.Entities;
 
@@ -7,4 +8,10 @@ namespace VoicePulse.Infrastructure.Persistence;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options) , IApplicationDbContext
 {
     public DbSet<Poll> Polls { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
