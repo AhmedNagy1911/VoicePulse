8c397ce2b57ae5e3c9b63fa99bf72f155b447b69
AhmedNagy1911 <zamalekahmed196@gmail.com>
2026-08-23 01:59:34 +0300

    Update Poll Entity

---
+++ VoicePulse.Domain/Entities/AuditableEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoicePulse.Domain.Entities;
+
+public class AuditableEntity
+{
+    public string CreatedById { get; set; } = string.Empty;
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+    public string? UpdatedById { get; set; }
+    public DateTime? UpdatedOn { get; set; }
+
+    public ApplicationUser CreatedBy { get; set; } = default!;
+    public ApplicationUser? UpdatedBy { get; set; }
+}
