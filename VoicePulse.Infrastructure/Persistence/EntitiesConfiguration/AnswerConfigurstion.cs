446d87ca76b8177101d1521f0ecf529d324dffc9
AhmedNagy1911 <zamalekahmed196@gmail.com>
2026-08-23 22:31:59 +0300

    Add Configuration to Answer and question entities

---
+++ VoicePulse.Infrastructure/Persistence/EntitiesConfiguration/AnswerConfigurstion.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VoicePulse.Domain.Entities;
+
+namespace VoicePulse.Infrastructure.Persistence.EntitiesConfiguration;
+
+public class AnswerConfigurstion : IEntityTypeConfiguration<Answer>
+{
+    public void Configure(EntityTypeBuilder<Answer> builder)
+    {
+        builder.HasIndex(x => new { x.QuestionId, x.Content }).IsUnique();
+
+        builder.Property(x => x.Content).HasMaxLength(1000);
+    }
+}
---
+++ VoicePulse.Infrastructure/Persistence/EntitiesConfiguration/QuestionConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VoicePulse.Domain.Entities;
+
+namespace VoicePulse.Infrastructure.Persistence.EntitiesConfiguration;
+
+public class QuestionConfiguration : IEntityTypeConfiguration<Question>
+{
+    public void Configure(EntityTypeBuilder<Question> builder)
+    {
+        builder.HasIndex(x => new { x.PollId, x.Content }).IsUnique();
+
+        builder.Property(x => x.Content).HasMaxLength(1000);
+    }
+}
