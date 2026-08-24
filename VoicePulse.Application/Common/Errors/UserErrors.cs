4b324e9fbfe1c04a86ea8ad0497fe1df983f7f27
AhmedNagy1911 <zamalekahmed196@gmail.com>
2026-08-24 20:45:06 +0300

    Code Refactoring

--- VoicePulse.Application/Common/Errors/UserErrors.cs
+++ VoicePulse.Application/Common/Errors/UserErrors.cs
@@ -3,12 +3,12 @@
 public static class UserErrors
 {
      public static readonly Error InvalidCredentials =
-        new("User.InvalidCredentials", "Invalid email/password" ,400);
+        new("User.InvalidCredentials", "Invalid email/password" ,401);
 
     public static readonly Error InvalidJwtToken =
-     new("User.InvalidJwtToken", "Invalid Jwt token" ,400);
+     new("User.InvalidJwtToken", "Invalid Jwt token" ,401);
 
     public static readonly Error InvalidRefreshToken =
-        new("User.InvalidRefreshToken", "Invalid refresh token",400);
+        new("User.InvalidRefreshToken", "Invalid refresh token",401);
 
 }
