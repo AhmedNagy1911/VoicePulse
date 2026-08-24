e0e8abd64590c220cd21085c5cbc743a3f2b32cd
AhmedNagy1911 <zamalekahmed196@gmail.com>
2026-08-24 20:33:24 +0300

    Add Save Vote Endpoint

--- VoicePulse.API/Controllers/VotesController.cs
+++ VoicePulse.API/Controllers/VotesController.cs
@@ -2,16 +2,19 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using VoicePulse.API.Extensions;
+using VoicePulse.Application.Contracts.Votes;
 using VoicePulse.Application.Interfaces;
+using VoicePulse.Application.Services;
 
 namespace VoicePulse.API.Controllers;
 
 [Route("api/polls/{pollId}/vote")]
 [ApiController]
 [Authorize]
-public class VotesController(IQuestionService questionService) : ControllerBase
+public class VotesController(IQuestionService questionService , IVoteService voteService ) : ControllerBase
 {
     private readonly IQuestionService _questionservice = questionService;
+    private readonly IVoteService _voteservice = voteService;
 
     [HttpGet("")]
     public async Task<IActionResult> Start([FromRoute] int pollId , CancellationToken cancellationToken)
@@ -22,6 +25,16 @@ public class VotesController(IQuestionService questionService) : ControllerBase
 
         return result.IsSuccess 
             ? Ok(result.Value)
+            : result.ToProblem();
+    }
+
+    [HttpPost("")]
+    public async Task<IActionResult> Vote([FromRoute] int pollId, [FromBody] VoteRequest request, CancellationToken cancellationToken)
+    {
+        var result = await _voteservice.AddAsync(pollId, User.GetUserId()!, request, cancellationToken);
+
+        return result.IsSuccess 
+            ? Created() 
             : result.ToProblem();
     }
 }
